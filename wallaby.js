d135e89c2849ac02da0e10260a1f1ef9616848c4
ArtemGovorov <artem.govorov@gmail.com>
2015-06-20 13:15:08 +1000

    Added VS project and workaround for requirejs

--- wallaby.js
+++ wallaby.js
@@ -1,21 +1,34 @@
-module.exports = function () {
-  return {
-    files: [
-      {load: true, instrument: false, pattern: 'bower_modules/requirejs/require.js'},
-      {load: true, instrument: true, pattern: 'src/config.js'},
-      {load: false, instrument: true, pattern: 'src/apps/StopWatch.js'},
-      {load: true, instrument: true, pattern: 'test/test-main-wallaby.js'}
-    ],
+module.exports = function (wallaby) {
 
-    tests: [
-      {load: false, instrument: true, pattern: 'src/apps/StopWatchTest.js'}
-    ],
+    var fs = require('fs');
+    var path = require('path');
+    var requirejsRelativePath = 'bower_modules/requirejs/require.js';
+    var requirejsCode = fs.readFileSync(path.join(wallaby.localProjectDir, requirejsRelativePath));
 
-    middleware: function (app, express) {
-      app.use('/bower_modules',
-        express.static(
-          require('path').join(__dirname, 'bower_modules')));
-    },
-    debug: true
-  };
+    return {
+        files: [
+          { load: true, instrument: true, pattern: 'src/config.js' },
+          { load: false, instrument: true, pattern: 'src/apps/StopWatch.js' },
+          { load: true, instrument: true, pattern: 'test/test-main-wallaby.js' }
+        ],
+
+        tests: [
+          { load: false, instrument: true, pattern: 'src/apps/StopWatchTest.js' }
+        ],
+
+        postprocessor: function (wallaby) {
+            return wallaby.createFile({
+                path: requirejsRelativePath,
+                content: requirejsCode,
+                order: -1,    // to make it the first file loaded in the sandbox
+                ts: 1         // fixed timestamp to cache it forever in phantomjs
+            })
+        },
+
+        middleware: function (app, express) {
+            app.use('/bower_modules', express.static(path.join(__dirname, 'bower_modules')));
+        },
+
+        debug: true
+    };
 };
